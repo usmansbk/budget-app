6838146301aaefc424bb0a3741061c5de882ce52
Usman Suleiman <usmansbk@gmail.com>
2021-11-16 12:30:52 +0100

    Add many to many relationship

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
   validates :name, :icon, presence: true, length: { maximum: 100 }
+
   belongs_to :user
-  belongs_to :deal
+  has_and_belongs_to_many :deals
 end
