5843a93a7cbbb9a0745baa2dfb8d6bb8d66fd978
Usman Suleiman <usmansbk@gmail.com>
2021-11-16 10:47:45 +0100

    Create deals table

--- db/migrate/20211116082103_create_deals.rb
+++ db/migrate/20211116082103_create_deals.rb
@@ -1,9 +1,9 @@
 class CreateDeals < ActiveRecord::Migration[6.1]
   def change
     create_table :deals do |t|
-      t.string, :name
+      t.string :name
       t.decimal :amount
-      t.references :user, null: false, foreign_key: true
+      t.references :author, references: :users, null: false, foreign_key: { to_table: :users }
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_15_205819) do
+ActiveRecord::Schema.define(version: 2021_11_16_082103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "deals", force: :cascade do |t|
+    t.string "name"
+    t.decimal "amount"
+    t.bigint "author_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_deals_on_author_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
@@ -28,4 +37,5 @@ ActiveRecord::Schema.define(version: 2021_11_15_205819) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "deals", "users", column: "author_id"
 end
