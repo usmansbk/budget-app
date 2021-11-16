88875011f94776ff0cde00401d508905ca349220
Usman Suleiman <usmansbk@gmail.com>
2021-11-16 21:36:13 +0100

    Fix typo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery prepent: true
+  protect_from_forgery prepend: true
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
