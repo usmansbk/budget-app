bfeedd6d1f8edbc377b407fbb42d19ffc68379e5
Usman Suleiman <usmansbk@gmail.com>
2021-11-16 21:26:18 +0100

    Setup cancancan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery prepent: true
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  load_and_authorize_resource
 
   protected
 
