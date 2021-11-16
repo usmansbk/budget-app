c5360ef1ffa8fc0ee45431c51cf178cf8ff46cec
Usman Suleiman <usmansbk@gmail.com>
2021-11-16 11:11:24 +0100

    Add validations

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,3 +1,5 @@
 class Deal < ApplicationRecord
   belongs_to :author, class_name: 'User'
+  validates :name, :amount, presence: true, length: { maximum: 100 }
+  validates :amount, numericality: { greater_than: 0 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, :amount, presence: true, length: { maximum: 100 }
   has_many :deals
 end
