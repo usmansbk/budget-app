96cef4096f47078280c882f97e5c6eb372a1a45b
Usman Suleiman <usmansbk@gmail.com>
2021-11-16 11:43:59 +0100

    Test model

--- spec/models/deal_spec.rb
+++ spec/models/deal_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Deal, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validates' do
+    subject { FactoryBot.build :deal }
+
+    it 'should have a name' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'should not have an empty name' do
+      subject.name = ' '
+      expect(subject).to_not be_valid
+    end
+
+    it 'should have an amount' do
+      subject.amount = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'should have an amount greater than 0' do
+      subject.amount = 0
+      expect(subject).to_not be_valid
+    end
+  end
+
+  describe 'author' do
+    subject { FactoryBot.build :deal }
+
+    it 'should have an author' do
+      expect(subject.author).to be_present
+    end
+  end
 end
