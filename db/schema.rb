d193e31cc04f50fb37d10be8a1f36a13bc020e56
Usman Suleiman <usmansbk@gmail.com>
2021-11-16 12:53:28 +0100

    Create join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_16_113910) do
+ActiveRecord::Schema.define(version: 2021_11_16_115254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 2021_11_16_113910) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "categories_deals", id: false, force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.bigint "deal_id", null: false
+    t.index ["category_id", "deal_id"], name: "index_categories_deals_on_category_id_and_deal_id"
   end
 
   create_table "deals", force: :cascade do |t|
