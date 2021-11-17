0eccaf13a60db5d3c8c8ba794c643700a240687d
Usman Suleiman <usmansbk@gmail.com>
2021-11-17 08:54:40 +0100

    Set splash screen

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   authenticated :user do
     root to: 'categories#index', as: :authenticated_root
   end
-  root to: redirect('categories#index')
+  root to: 'splash#index'
+
   devise_for :users
 
   resources :categories
-  get 'welcome', to: 'splash#index'
 end
