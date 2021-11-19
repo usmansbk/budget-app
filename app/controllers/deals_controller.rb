f01796bd170b092159c0d7111f035144081c5faf
Usman Suleiman <usmansbk@gmail.com>
2021-11-19 08:42:37 +0100

    Set categories

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -3,15 +3,18 @@ class DealsController < ApplicationController
 
   def new
     @deal = Deal.new
+    set_categories
   end
 
   def create
     @deal = Deal.new(deal_params)
+    @deal.author = current_user
 
     respond_to do |format|
       if @deal.save
-        format.html { redirect_to @deal, notice: 'Deal was successfully created.' }
+        format.html { redirect_to @deal, notice: 'Transaction was successfully added.' }
       else
+        set_categories
         format.html { render :new, status: :unprocessable_entity }
       end
     end
@@ -19,7 +22,11 @@ class DealsController < ApplicationController
 
   private
 
+  def set_categories
+    @categories = current_user.categories.map { |category| [category.name, category.id] }
+  end
+
   def deal_params
-    params.fetch(:deal, {})
+    params.fetch(:deal, {}).permit(:name, :amount)
   end
 end
