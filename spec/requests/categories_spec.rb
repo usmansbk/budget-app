36a30cf0282f00a109b10be33b7e1b3f85fd18c2
Usman Suleiman <usmansbk@gmail.com>
2021-11-19 16:00:00 +0100

    Setup request test

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -1,19 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe '/categories', type: :request do
-  # Category. As you add validations to Category, be sure to
-  # adjust the attributes here as well.
-  let(:valid_attributes) do
-    skip('Add a hash of attributes valid for your model')
-  end
-
-  let(:invalid_attributes) do
-    skip('Add a hash of attributes invalid for your model')
-  end
+  login_user
 
   describe 'GET /index' do
     it 'renders a successful response' do
-      Category.create! valid_attributes
+      FactoryBot.create(:category)
       get categories_url
       expect(response).to be_successful
     end
@@ -21,7 +13,7 @@ RSpec.describe '/categories', type: :request do
 
   describe 'GET /show' do
     it 'renders a successful response' do
-      category = Category.create! valid_attributes
+      category = FactoryBot.create(:category)
       get category_url(category)
       expect(response).to be_successful
     end
@@ -38,12 +30,12 @@ RSpec.describe '/categories', type: :request do
     context 'with valid parameters' do
       it 'creates a new Category' do
         expect do
-          post categories_url, params: { category: valid_attributes }
+          post categories_url, params: { category: FactoryBot.create(:category) }
         end.to change(Category, :count).by(1)
       end
 
       it 'redirects to the created category' do
-        post categories_url, params: { category: valid_attributes }
+        post categories_url, params: { category: FactoryBot.create(:category) }
         expect(response).to redirect_to(category_url(Category.last))
       end
     end
@@ -51,12 +43,12 @@ RSpec.describe '/categories', type: :request do
     context 'with invalid parameters' do
       it 'does not create a new Category' do
         expect do
-          post categories_url, params: { category: invalid_attributes }
+          post categories_url, params: { category: FactoryBot.create(:category) }
         end.to change(Category, :count).by(0)
       end
 
       it "renders a successful response (i.e. to display the 'new' template)" do
-        post categories_url, params: { category: invalid_attributes }
+        post categories_url, params: { category: FactoryBot.create(:category) }
         expect(response).to be_successful
       end
     end
@@ -64,14 +56,14 @@ RSpec.describe '/categories', type: :request do
 
   describe 'DELETE /destroy' do
     it 'destroys the requested category' do
-      category = Category.create! valid_attributes
+      category = FactoryBot.create(:category)
       expect do
         delete category_url(category)
       end.to change(Category, :count).by(-1)
     end
 
     it 'redirects to the categories list' do
-      category = Category.create! valid_attributes
+      category = FactoryBot.create(:category)
       delete category_url(category)
       expect(response).to redirect_to(categories_url)
     end
